6fb9fd1ea5374ec27c053be5743ad405acc952db
Salome2112 <146013347+Salome2112@users.noreply.github.com>
2024-06-25 19:48:02 -0500

    nuevo

---
+++ blog-video.md
@@ -0,0 +1,1 @@
+# https://youtu.be/JqaKumU3oh0
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'numeros-primos' title`, () => {
+  it(`should have the 'My-blog' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('numeros-primos');
+    expect(app.title).toEqual('my-blog');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, numeros-primos');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, My-blog');
   });
 });
